27afed24b20525a47cb90f5646f7b933d4cd06ea
shyambandla <shyambandla143@gmail.com>
2021-09-26 17:20:17 +0530

    hii

--- index.js
+++ index.js
@@ -14,18 +14,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 var ssocket;
+global.online=0;
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+  global.online+=1;
+console.log(global.online);
+  socket.broadcast.emit('online',global.online);
  // socket.emit('chat message',"hello");
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    global.online-=1;
+    socket.broadcast.emit('online',global.online);
   });
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
     socket.broadcast.emit('chat message',msg);
     
   });
-  ssocket=socket;
+  socket.on('admin',(msg)=>{
+    socket.broadcast.emit('admin',msg);
+    
+  }); 
+
+ 
 });
 
 
